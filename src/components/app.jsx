674f6765792a2a63227c006c27f1030cfe7cbee3
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-06-17 09:19:00 -0500

    lab completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,59 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App(props) {
-    return <h1>dis dat string</h1>
-};
+// const App = (props) => {
+//     return (
+//     <h1>{props.text}</h1>
+//     )
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholderText: "Your Name",
+      input: "",
+      hasLoaded: false,
+    };
+  }
+
+  handleInputChange = (value) => {
+    this.setState({ input: value });
+  };
+
+  handleClick = () => {
+    if (this.state.hasLoaded === false) {
+      this.setState({ hasLoaded: true });
+    } else {
+      this.setState({ hasLoaded: false });
+    }
+  };
+
+  componentDidMount = () => {
+    this.setState({hasLoaded: true})
+  }
+
+  render() {
+    if (this.state.hasLoaded === false) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+          <button onClick={() => this.handleClick()}>Toggle</button>
+        </div>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Hi there</h1>
+          <input
+            placeholder={this.state.placeholderText}
+            value={this.state.value}
+            onChange={(event) => this.handleInputChange(event.target.value)}
+          />
+          <button onClick={() => this.handleClick()}>Toggle</button>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
 export default App;
